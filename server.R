7f15785b6c693c3b702837276fcb113fe1728f55
Chennade Brown <cjbrow25@ncsu.edu>
2021-11-21 01:37:40 -0500

    I have two widgets

--- server.R
+++ server.R
@@ -1,26 +1,49 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(caret)
+library(tidyverse)
+library(DT)
+library(knitr)
+library(GGally)
+library(dplyr)
+library(caret)
+library(tree)
+library(rpart)
+library(rattle)
+employData <- read_csv("EmployeeAttrition.csv")
 
-# Define server logic required to draw a histogram
+# Clean Data.
+# Remove non predictor variables from the data set.
+employData$EmployeeCount <- NULL
+employData$EmployeeNumber <- NULL
+employData$StandardHours <- NULL
+
+
+
+# Store categorical and numeric variables in separate data frames for automation of plots.
+numericVars <- select(employData, Age, Attrition, TotalWorkingYears, MonthlyIncome, YearsAtCompany, PercentSalaryHike, TrainingTimesLastYear, YearsInCurrentRole, DailyRate, DistanceFromHome, HourlyRate, MonthlyRate, NumCompaniesWorked, YearsSinceLastPromotion, YearsWithCurrManager)
+
+# Create binary variable Attrition2 for scatterplots.
+numericVars <- numericVars %>% mutate(Attrition2 = ifelse(Attrition == "No", "0", "1"))
+numericVars$Attrition <- as.factor(numericVars$Attrition)
+
+# Remove categorical Attrition.
+numericVars$Attrition <- NULL
+numericVars
+
+catVars <- select(employData, Attrition, Department, JobRole, Gender, EducationField, BusinessTravel, EnvironmentSatisfaction, JobInvolvement, JobLevel, JobSatisfaction, MaritalStatus, Over18, OverTime, PerformanceRating, RelationshipSatisfaction, StockOptionLevel, WorkLifeBalance)
+
+# Store the predictors and the response variables in vectors.
+catPred = names(catVars)[1:17]
+
+# Name the vectors.
+catPred = set_names(catPred)
+
+# Change categorical variables to factors for plotting.
+factors <- c("Attrition" ,"Department", "JobRole", "Gender", "EducationField", "BusinessTravel", "EnvironmentSatisfaction", "JobInvolvement", "JobLevel", "JobSatisfaction", "MaritalStatus", "Over18", "OverTime", "PerformanceRating", "RelationshipSatisfaction", "StockOptionLevel", "WorkLifeBalance")
+catData <- catVars
+catData[,factors] <- lapply(catData[,factors] , factor)
+
+
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+  
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,28 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(caret)
 library(shiny)
+library(DT)
+library(knitr)
+library(GGally)
+library(dplyr)
+library(caret)
+library(tree)
+library(rpart)
+library(rattle)
+library(tidyverse)
+employData <- read_csv("EmployeeAttrition.csv")
 
-# Define UI for application that draws a histogram
+
+
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+  titlePanel("Predicting Employee Attrition"),
+  sidebarLayout(
+    sidebarPanel(
+      h4("Select Categorical or Numeric Predictors for Plots:"),
+    selectInput("catVariable", label = "Categorical Predictors", selected = "Attrition", choices = catData),
+    selectInput("numVariables",label = "Numeric Predictors", selected = "Age", choices = numericVars),
+  ),
+    mainPanel(
     )
-))
+  )
+)
+)
